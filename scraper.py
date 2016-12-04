377a57876383b9124e7e58cbbcb32da8cc3ce975
Dewitt Buckingham <dj729@comcast.net>
2016-12-04 00:22:56 -0800

    Adapt to work for Web Development Gigs

--- scraper.py
+++ scraper.py
@@ -1,15 +1,15 @@
-from craigslist import CraigslistHousing
+from craigslist import CraigslistGigs
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean
 from sqlalchemy.orm import sessionmaker
 from dateutil.parser import parse
-from util import post_listing_to_slack, find_points_of_interest
+from util import post_listing_to_slack
 from slackclient import SlackClient
 import time
 import settings
 
-engine = create_engine('sqlite:///listings.db', echo=False)
+engine = create_engine('sqlite:///gigs.db', echo=False)
 
 Base = declarative_base()
 
@@ -18,20 +18,14 @@ class Listing(Base):
     A table to store data on craigslist listings.
     """
 
-    __tablename__ = 'listings'
+    __tablename__ = 'gigs'
 
     id = Column(Integer, primary_key=True)
     link = Column(String, unique=True)
     created = Column(DateTime)
-    geotag = Column(String)
-    lat = Column(Float)
-    lon = Column(Float)
     name = Column(String)
-    price = Column(Float)
     location = Column(String)
     cl_id = Column(Integer, unique=True)
-    area = Column(String)
-    bart_stop = Column(String)
 
 Base.metadata.create_all(engine)
 
@@ -44,11 +38,11 @@ def scrape_area(area):
     :param area:
     :return: A list of results.
     """
-    cl_h = CraigslistHousing(site=settings.CRAIGSLIST_SITE, area=area, category=settings.CRAIGSLIST_HOUSING_SECTION,
-                             filters={'max_price': settings.MAX_PRICE, "min_price": settings.MIN_PRICE})
+    cl_h = CraigslistGigs(site=settings.CRAIGSLIST_SITE, area=area, category=settings.CRAIGSLIST_CATEGORY,
+                             filters={'is_paid': 'yes', 'query': 'website'})
 
     results = []
-    gen = cl_h.get_results(sort_by='newest', geotagged=True, limit=20)
+    gen = cl_h.get_results(sort_by='newest', limit=20)
     while True:
         try:
             result = next(gen)
@@ -62,50 +56,23 @@ def scrape_area(area):
         if listing is None:
             if result["where"] is None:
                 # If there is no string identifying which neighborhood the result is from, skip it.
-                continue
-
-            lat = 0
-            lon = 0
-            if result["geotag"] is not None:
-                # Assign the coordinates.
-                lat = result["geotag"][0]
-                lon = result["geotag"][1]
-
-                # Annotate the result with information about the area it's in and points of interest near it.
-                geo_data = find_points_of_interest(result["geotag"], result["where"])
-                result.update(geo_data)
-            else:
-                result["area"] = ""
-                result["bart"] = ""
-
-            # Try parsing the price.
-            price = 0
-            try:
-                price = float(result["price"].replace("$", ""))
-            except Exception:
-                pass
+                result["where"] = "N/A"
 
             # Create the listing object.
             listing = Listing(
                 link=result["url"],
                 created=parse(result["datetime"]),
-                lat=lat,
-                lon=lon,
                 name=result["name"],
-                price=price,
                 location=result["where"],
-                cl_id=result["id"],
-                area=result["area"],
-                bart_stop=result["bart"]
+                cl_id=result["id"]
             )
 
             # Save the listing so we don't grab it again.
             session.add(listing)
             session.commit()
 
-            # Return the result if it's near a bart station, or if it is in an area we defined.
-            if len(result["bart"]) > 0 or len(result["area"]) > 0:
-                results.append(result)
+            # Return the results
+            results.append(result)
 
     return results
 
